6091569912913203e56e483dc29b324ed0eb8d5b
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-04-10 06:27:13 +0300

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Header from "../components/Header";
 import Name from "../components/Name";
 import Achievements from "../components/Achievements";
@@ -6,13 +7,18 @@ import Separator from "../components/Separator";
 
 const Home: NextPage = () => {
   return (
-    <div className="font-[SFUI] text-[20px]">
-      <Header />
-      <Separator />
-      <Name />
-      <Separator />
-      <Achievements />
-    </div>
+    <>
+      <Head>
+        <title>Made By Baraa</title>
+      </Head>
+      <div className="font-[SFUI] text-[20px]">
+        <Header />
+        <Separator />
+        <Name />
+        <Separator />
+        <Achievements />
+      </div>
+    </>
   );
 };
 
