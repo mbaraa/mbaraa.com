eadbeb052587ce2f6da05b687e49e77d11ff9454
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-08-19 15:16:02 +0300

    some SEO stuff

--- client/src/lib/components/Name.svelte
+++ client/src/lib/components/Name.svelte
@@ -2,6 +2,6 @@
     class="relative bg-[#673AB7] h-[125px] md:h-[175px] font-[SFUI] text-[35px] md:text-[55px] font-black text-gray-50 md:pl-[40px]"
 >
     <span class="absolute top-[50%] translate-y-[-50%] text-center w-full">
-        <a title="Click to checkout my blog!" href="/blog"> Baraa Al-Masri </a>
+        <a title="Click to checkout my blog!" href="/"> Baraa Al-Masri </a>
     </span>
 </div>
---
+++ client/src/routes/blog/+layout.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Made By Baraa - Blog</title>
+</svelte:head>
+
+<slot />
