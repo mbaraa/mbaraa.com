4494b9d66e1fa652deba1c79d8c73247627d7a31
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-02-09 07:52:30 +0000

    added gentoo projects

--- src/lib/utils/requests/ProjectRequests.ts
+++ src/lib/utils/requests/ProjectRequests.ts
@@ -5,6 +5,55 @@ export default class ProjectRequests {
   static async getProjectGroups(): Promise<ProjectGroup[]> {
     return [
       {
+        name: "Google Developer Student Clubs",
+        description: "Stuff for GDSC - ASU to solve some problems",
+        projects: [
+          {
+            name: "GDSC - ASU Home Page",
+            description:
+              "A home page for the GDSC - ASU chapter, that can be customized easily and reused for any other GDSC chapter.",
+            startYear: "2022",
+            endYear: "2023",
+            website: "https://gdscasu.com",
+            sourceCode: "https://github.com/GDSC-ASU/website",
+            imagePath: `/images/gdg.png`,
+          },
+          {
+            name: "GDSC Logo Generator",
+            description:
+              "My first web project, my Google Developer Student Clubs chapter's lead thought it would be a great idea if we had a logo generator that every other GDSC chapters can use it, so that every GDSC logos look the same in a neat way.",
+            startYear: "2020",
+            endYear: "2021",
+            website: "https://logogen.gdscasu.com",
+            sourceCode: "https://github.com/mbaraa/dsc_logo_generator",
+            imagePath: `/images/logogen.png`,
+          },
+        ],
+      },
+      {
+        name: "Gentoo Related",
+        description: "Stuff that helps with Gentoo Linux",
+        projects: [
+          {
+            name: "Eloi",
+            description:
+              "Gentoo ebuilds finder and installer, finds ebuilds from various overlay repos, and makes the needed cahnges to install an ebuild.",
+            startYear: "2023",
+            sourceCode: "https://github.com/mbaraa/eloi",
+            imagePath: "/images/gentoo.svg",
+          },
+          {
+            name: "Schwifter",
+            description:
+              "A Gentoo post installer script, inspired from Helmuthdu's AUI, [needs updates for new packages' versions]",
+            startYear: "2019",
+            endYear: "2019",
+            sourceCode: "https://github.com/mbaraa/schwifter",
+            imagePath: "/images/gentoo.svg",
+          },
+        ],
+      },
+      {
         name: "College Related",
         description: "Stuff for my faculty",
         projects: [
@@ -39,43 +88,16 @@ export default class ProjectRequests {
         ],
       },
       {
-        name: "Google Developer Student Clubs",
-        description: "Stuff for GDSC - ASU to solve some problems",
-        projects: [
-          {
-            name: "GDSC - ASU Home Page",
-            description:
-              "A home page for the GDSC - ASU chapter, that can be customized easily and reused for any other GDSC chapter.",
-            startYear: "2022",
-            endYear: "2023",
-            website: "https://gdscasu.com",
-            sourceCode: "https://github.com/GDSC-ASU/website",
-            imagePath: `/images/gdg.png`,
-          },
-          {
-            name: "GDSC Logo Generator",
-            description:
-              "My first web project, my Google Developer Student Clubs chapter's lead thought it would be a great idea if we had a logo generator that every other GDSC chapters can use it, so that every GDSC logos look the same in a neat way.",
-            startYear: "2020",
-            endYear: "2021",
-            website: "https://logogen.gdscasu.com",
-            sourceCode: "https://github.com/mbaraa/dsc_logo_generator",
-            imagePath: `/images/logogen.png`,
-          },
-        ],
-      },
-      {
         name: "Misc Web",
         description: "Some other web stuff",
         projects: [
           {
             name: "Ladder and Snake",
-            description:
-              "A weird looking Ladder and Snake game.",
+            description: "A weird looking Ladder and Snake game.",
             startYear: "2022",
             endYear: "2022",
             sourceCode: "https://github.com/mbaraa/ladder_and_snake",
-            imagePath: "/images/ladder-and-snake.png",  
+            imagePath: "/images/ladder-and-snake.png",
           },
           {
             name: "Temco MEP Home Page",
