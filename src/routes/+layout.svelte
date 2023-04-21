584153dae4852d9392901f8724394e909db7c7cf
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-04-21 06:10:52 +0300

    moved header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
-	import '../app.css';
+<script lang="ts">
+	import "../app.css";
+	import Header from "$lib/components/Header.svelte";
+	import type Link from "$lib/models/Link";
+	import type Info from "$lib/models/Info";
+
+	export let data: { links: Link[]; info: Info };
 </script>
 
+<Header title={data.info.name} showTitle={false} showSeparator={false} links={data.links} />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-	import Header from "$lib/components/Header.svelte";
 	import Bio from "$lib/components/home/Bio.svelte";
-	import type Link from "$lib/models/Link";
 	import type Info from "$lib/models/Info";
 
-	export let data: { links: Link[]; info: Info };
+	export let data: { info: Info };
 </script>
 
 <svelte:head>
 	<title>Home of Baraa</title>
 </svelte:head>
 
-<div class="">
-	<Header title={data.info.name} showTitle={false} showSeparator={false} links={data.links} />
-	<Bio name={data.info.name} brief={data.info.brief} />
-</div>
+<Bio name={data.info.name} brief={data.info.brief} />
