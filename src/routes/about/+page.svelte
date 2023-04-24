1ec169060562853f896bbd47306cd76510a53d3d
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-04-25 01:15:15 +0300

    ratio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,7 +11,7 @@
 <div class="font-[Vistol]">
 	{#if data}
 		<div
-				class="w-[80%] md:w-[50%] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]"
+			class="h-[90vh] sm:h-auto my-[30px] sm:my-[70px] py-[70px] sm:py-[100px] w-[90%] sm:w-[93%] xl:w-[70%] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] z-[-1]"
 		>
 			<h1 class="text-[25px] font-bold text-white">About Me</h1>
 			<p class="text-[#20DB8F] text-[20px]">{data.info.about}</p>
