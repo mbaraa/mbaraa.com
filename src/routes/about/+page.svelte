df17ab4418ba1296835db25030bc10e480f9d6c6
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-07-12 14:38:53 +0300

    separated technologies list into chunks

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import MarkdownViewer from "$lib/components/MarkdownViewer.svelte";
 	import type Info from "$lib/models/Info";
 
 	export let data: { info: Info };
+	let techSizePerChunk = Math.ceil(data.info.technologies.length / 3);
+	const bigScreenTechLists = new Array<Array<string>>();
+	for (let i = 0; i < data.info.technologies.length; i += techSizePerChunk) {
+		const chunk = new Array<string>();
+		for (let j = i; j < i + techSizePerChunk; j++) {
+			chunk.push(data.info.technologies[j]);
+		}
+		bigScreenTechLists.push(chunk);
+	}
 </script>
 
 <svelte:head>
@@ -10,17 +20,27 @@
 
 <div class="font-[Vistol]">
 	{#if data}
-		<div
-			class="h-[90vh] sm:h-auto my-[30px] sm:my-[70px] py-[70px] sm:py-[100px] w-[90%] sm:w-[93%] xl:w-[70%] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] z-[-1]"
-		>
-			<h1 class="text-[25px] font-bold text-white">About Me</h1>
-			<p class="text-[#20DB8F] text-[20px]">{data.info.about}</p>
-			<h2 class="text-[#ABABAB] text-[17px] mt-[10px]">Some of the technologies that I've used:</h2>
-			<ul class="list-disc">
-				{#each data.info.technologies as technology}
-					<li class="ml-[15px] text-white">{technology}</li>
-				{/each}
-			</ul>
+		<div class="h-[90vh] sm:h-auto">
+			<div class="px-10 py-5 lg:p-20">
+				<h1 class="text-[25px] font-bold text-white">About Me</h1>
+				<p class="text-[#20DB8F] text-[20px]"><MarkdownViewer text={data.info.about} /></p>
+				<hr class="my-10" />
+				<h2 class="text-[#ABABAB] text-[17px] mb-3">Some of the technologies that I've used:</h2>
+				<ul class="list-disc block lg:hidden">
+					{#each data.info.technologies as technology}
+						<li class="ml-[15px] text-white">{technology}</li>
+					{/each}
+				</ul>
+				<div class="hidden lg:flex gap-32">
+					{#each bigScreenTechLists as technologyChunk}
+						<ul class="list-disc inline">
+							{#each technologyChunk as technology}
+								<li class="ml-[15px] text-white">{technology}</li>
+							{/each}
+						</ul>
+					{/each}
+				</div>
+			</div>
 		</div>
 	{/if}
 </div>
