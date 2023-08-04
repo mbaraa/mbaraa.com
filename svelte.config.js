2d5be54b5cd9e89896daf4f0cf830fe89811101d
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-08-04 16:36:02 +0300

    added a route for the upload/download process

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false
+		}
 	}
 };
 
