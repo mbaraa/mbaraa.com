90a8ca505401f00907e781046f29d217233788ff
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-08-04 18:48:30 +0300

    fixed jwt imports thingy pt.2

--- src/routes/api/_auth.ts
+++ src/routes/api/_auth.ts
@@ -1,6 +1,8 @@
-import { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import config from "$lib/config";
 import type { RequestEvent } from "@sveltejs/kit";
+
+const { verify } = jwt;
 
 export function isAuth(ev: RequestEvent): boolean {
 	const token = ev.request.headers.get("Authorization") ?? "";
